33e23ac69aed706d945942558fc7f7f4222d6164
Ngogoyo Michael <michaeln1486@gmail.com>
2025-09-21 20:51:16 +0300

    Update application configuration and add deployment script

---
+++ posBackend/deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -e
+
+# === config ===
+
+JAR_NAME="pos-0.0.1-SNAPSHOT.jar"
+REMOTE_USER="michael"
+REMOTE_HOST="20.238.27.70"
+REMOTE_DIRECTORY="/opt/pos"
+
+#=== Build Jar ===
+echo "Building the project..."
+./mvnw clean package -DskipTests
+
+#=== upload jar ===
+echo "Uploading the jar to ${REMOTE_USER}@${REMOTE_HOST}:${REMOTE_DIRECTORY}..."
+scp "target/$JAR_NAME"  "$REMOTE_USER@$REMOTE_HOST:$REMOTE_DIRECTORY/"
+
+#=== restart service ===
+echo "Restarting the service on the remote server..."
+ssh "$REMOTE_USER@$REMOTE_HOST" << EOF
+    pkill -f "$JAR_NAME" || true
+    nohup java -jar "$REMOTE_DIRECTORY/$JAR_NAME" > "$REMOTE_DIRECTORY/app.log" 2>&1 &
+    echo "App started. logs: $REMOTE_DIRECTORY/app.log"
+EOF
+
--- posFrontend/src/App.jsx
+++ posFrontend/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
 
                 <Route path={ROUTES.ERROR}element={
                   <Error/>
-                }/>
+                }/> 
 
 
             </Routes>
