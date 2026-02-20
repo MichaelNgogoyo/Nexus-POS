cdc4a83427dcf64c7eb9aefd02c21ddac685dbf3
Michael Ngogoyo <michaeln1486@gmail.com>
2026-02-20 14:40:26 +0300

    Ensure category service is scanned

--- posBackend/src/main/java/com/pos/PosApplication.java
+++ posBackend/src/main/java/com/pos/PosApplication.java
@@ -1,7 +1,10 @@
 package com.pos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
 @SpringBootApplication
+@ComponentScan(basePackages = "com.pos")
 public class PosApplication {
     public static void main(String[] args) {
         SpringApplication.run(PosApplication.class, args);
